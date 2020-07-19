64b2a1d27ad1652834fc2102d613107158d3d313
Namrata <911npm@gmail.com>
2020-07-19 14:17:51 +0530

    Flipped coin multiple times and displayed winning percentage for singlet combinantion

--- flipCoinCombinantion.sh
+++ flipCoinCombinantion.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-flipCoin=$(( RANDOM%2 ))
-if [ $flipCoin -eq 1 ]
-then
-   echo "Head"
-else
-   echo "tail"
-fi
+declare -A singlet
+H=0
+T=0
+for step in {1..20}
+do
+	flipCoin=$(( RANDOM%2 ))
+	if [ $flipCoin -eq 1 ]
+	then
+		H=$(( $H+1 ))
+   	#echo "Head"
+	else
+		T=$(( $T+1 ))
+   	#echo "tail"
+	fi
+done
+
+singlet["H"]=$(( ($H*100)/20 ))
+singlet["T"]=$(( ($T*100)/20 ))
+
+for key in ${!singlet[@]}
+do
+	echo "Win Percentage for singlet combination $key is ${singlet[$key]}"
+done
+
