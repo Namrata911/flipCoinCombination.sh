d18231bec06e7cd9931852c4bc1f57c8d3dbaa37
Namrata <911npm@gmail.com>
2020-07-19 14:49:21 +0530

    Added code for doublet and displayed doublet combination with winning percentage

--- flipCoinCombinantion.sh
+++ flipCoinCombinantion.sh
@@ -23,3 +23,39 @@ do
 	echo "Win Percentage for singlet combination $key is ${singlet[$key]}"
 done
 
+echo "Flipping Two coins now "
+declare -A doublet
+HH=0
+HT=0
+TH=0
+TT=0
+for step in {1..20}
+do
+   flipCoin1=$(( RANDOM%2 ))
+	flipCoin2=$(( RANDOM%2 ))
+   combination=$flipCoin1$flipCoin2
+   case $combination in
+   11)
+      HH=$(( $HH+1 ))
+   ;;
+   10)
+      HT=$(( $HT+1 ))
+   ;;
+   01)
+      TH=$(( $TH+1 ))
+   ;;
+   00)
+      TT=$(( $TT+1 ))
+   ;;
+	esac
+done
+
+doublet["HH"]=$(( ($HH*100)/20 ))
+doublet["HT"]=$(( ($HT*100)/20 ))
+doublet["TH"]=$(( ($TH*100)/20 ))
+doublet["TT"]=$(( ($TT*100)/20 ))
+
+for key in ${!doublet[@]}
+do
+   echo "Win Percentage for doublet combination $key is ${doublet[$key]}"
+done
